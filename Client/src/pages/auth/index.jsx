5f6377036c13e83ce1cc0c9d5aba3ead944526cc
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-07-17 11:52:27 +0530

    Login Signup and Redirect

--- Client/src/pages/auth/index.jsx
+++ Client/src/pages/auth/index.jsx
@@ -6,11 +6,14 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { apiClient } from "@/lib/api-client";
-import { SIGNUP_ROUTE } from "@/utils/constants";
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants";
+import { useNavigate } from "react-router-dom";
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const validateSignup = () => {
     if (!email.length) {
@@ -27,21 +30,73 @@ function Auth() {
     }
     return true;
   };
-  const handleLogin = async () => {};
-  const handleSignup = async () => {
-    if (validateSignup()) {
-      const response = await apiClient.post(SIGNUP_ROUTE, {
-        email,
-        password,
-      });
-      console.log({ response });
+  const validateLogin = () => {
+    if (!email.length) {
+      toast.error("Email is required");
+      return false;
+    }
+    if (!password.length) {
+      toast.error("Password is required");
+      return false;
+    }
+    return true;
+  };
+  const handleLogin = async () => {
+    if (validateLogin()) {
+      const response = await apiClient.post(
+        LOGIN_ROUTE,
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true, // Required to recieved cookie from server
+        }
+      );
+
+      // if User exist
+
+      if (response.data.user.id) {
+        toast.success("Logged in successfully.");
+        //Checking if user has done profile setup
+        if (response.data.user.profileSetup) {
+          navigate("/chat");
+        } else {
+          navigate("/profile");
+        }
+      }
     }
   };
+
+  const handleSignup = async () => {
+    if (validateSignup()) {
+      const response = await apiClient.post(
+        SIGNUP_ROUTE,
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true, // Required to recieved cookie from server
+        }
+      );
+      if (response.status === 201) {
+        toast.success("Account created successfully");
+        // Redirecting user to complete their profile
+        navigate("/profile");
+      }
+
+      // //if account is not created then
+      // console.log(response.status);
+      // toast.error(response.statusText);
+    }
+  };
+
   return (
     <div className="h-[100vh] w-[100vw] flex justify-center items-center">
-      <div className="h-[90vh] w-[90vw] shadow-2xl border-white border-2 text-opacity-90 m:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2 overflow-auto ">
-        <div className="flex flex-col gap-10 items-center justify-center pl-10">
-          <div className="flex flex-col items-center justify-center">
+      <div className="h-[90vh] w-[90vw] shadow-2xl border-white border-2 text-opacity-90 m:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2 overflow-auto px-10 ">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex flex-col items-center justify-center ">
             <div className="flex items-center justify-center">
               <h1 className="text-3xl font-bold lg:text-5xl">Welcome</h1>
               <img className="h-[100px]" src={Victory} />
@@ -51,7 +106,7 @@ function Auth() {
             </p>
           </div>
           <div className="flex items-center justify-center w-full">
-            <Tabs className="w-3/4">
+            <Tabs className="w-3/4" defaultValue="login">
               <TabsList className="w-full bg-transparent rounded-none">
                 <TabsTrigger
                   className="data-[state=active]:bg-transparent text-gray-600 text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-blue-500 p-3 transition-all duration-300"
@@ -66,45 +121,45 @@ function Auth() {
                   Signup
                 </TabsTrigger>
               </TabsList>
-              <TabsContent className="flex gap-5 mt-10 flex-col" value="login">
+              <TabsContent className="flex gap-2 mt-5 flex-col" value="login">
                 <Input
                   placeholder="Email"
                   type="email"
                   value={email}
-                  className="p-6 rounded-full"
+                  className="p-6 rounded-2xl"
                   onChange={(event) => setEmail(event.target.value)}
                 />
                 <Input
                   placeholder="Password"
                   value={password}
                   type="password"
-                  className="p-6 rounded-full"
+                  className="p-6 rounded-2xl"
                   onChange={(event) => setPassword(event.target.value)}
                 />
                 <Button className="rounded-full p-6" onClick={handleLogin}>
                   Login
                 </Button>
               </TabsContent>
-              <TabsContent className="flex gap-5 flex-col" value="signup">
+              <TabsContent className="flex gap-2 flex-col mt-0" value="signup">
                 <Input
                   placeholder="Email"
                   type="email"
                   value={email}
-                  className="p-6 rounded-full"
+                  className="p-6 rounded-2xl"
                   onChange={(event) => setEmail(event.target.value)}
                 />
                 <Input
                   placeholder="Password"
                   value={password}
                   type="password"
-                  className="p-6 rounded-full"
+                  className="p-6 rounded-2xl"
                   onChange={(event) => setPassword(event.target.value)}
                 />
                 <Input
                   placeholder="Confirm Password"
                   value={confirmPassword}
                   type="password"
-                  className="p-6 rounded-full"
+                  className="p-6 rounded-2xl"
                   onChange={(event) => setConfirmPassword(event.target.value)}
                 />
                 <Button className="rounded-full p-6" onClick={handleSignup}>
--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
+import { compare } from "bcrypt";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
@@ -8,6 +9,7 @@ const createToken = (email, userID) => {
     expiresIn: maxAge,
   });
 };
+
 export const signup = async (request, response, next) => {
   try {
     const { email, password } = request.body;
@@ -23,7 +25,58 @@ export const signup = async (request, response, next) => {
       sameSite: "None",
     });
 
-    return response.status(201).json({ user });
+    // User created code 201
+    return response.status(201).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const login = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.status(400).send("Email and Password is required");
+    }
+    // Checking if user exist with this email
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return response.status(404).send("User not found with this email");
+    }
+
+    //Checking if password is correct using Bcrypt
+    const checkPassword = await compare(password, user.password);
+
+    if (!checkPassword) {
+      return response.status(400).send("Incorrect Password");
+    }
+
+    // Creating token and attaching it in cookie
+    response.cookie("jwt", createToken(email, user.id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    // sending user info back when sucess code 200
+    return response.status(200).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+        color: user.color,
+      },
+    });
   } catch (error) {
     console.log({ error });
     return response.status(500).send("Internal Server Error");
