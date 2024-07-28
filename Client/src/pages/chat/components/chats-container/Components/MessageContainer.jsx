e444230c9e9844fafb904d9a86fdf24cdc354911
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-07-28 22:08:17 +0530

    Image preview and download

--- Client/src/pages/chat/components/chats-container/Components/MessageContainer.jsx
+++ Client/src/pages/chat/components/chats-container/Components/MessageContainer.jsx
@@ -2,9 +2,10 @@ import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store/store";
 import { GET_MESSAGES_ROUTE, HOST } from "@/utils/constants";
 import moment from "moment";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdFolderZip } from "react-icons/md";
 import { IoMdArrowRoundDown } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 function MessageContainer() {
   const {
@@ -15,6 +16,8 @@ function MessageContainer() {
     setSelectedChatMessages,
   } = useAppStore();
   const scrollRef = useRef();
+  const [showImage, setShowImage] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
 
   useEffect(() => {
     const getMessages = async () => {
@@ -49,7 +52,7 @@ function MessageContainer() {
         responseType: "blob",
       });
 
-      const urlBlob = window.URL.createObjectURL(new Blocks([response.data]));
+      const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = urlBlob;
       link.setAttribute("download", file.split("/").pop());
@@ -86,7 +89,13 @@ function MessageContainer() {
             className={`${chatStyle} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
           >
             {checkIfImage(message.fileUrl) ? (
-              <div className="cursor-pointer">
+              <div
+                onClick={() => {
+                  setShowImage(true);
+                  setImageUrl(message.fileUrl);
+                }}
+                className="cursor-pointer"
+              >
                 <img
                   src={`${HOST}/${message.fileUrl}`}
                   height="300"
@@ -147,6 +156,33 @@ function MessageContainer() {
     <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
       {renderMessages()}
       <div ref={scrollRef}></div>
+      {showImage && (
+        <div className="fixed z-[1000] top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center backdrop-blur-lg flex-col">
+          <div>
+            <img
+              src={`${HOST}/${imageUrl}`}
+              className="h-[80vh] w-full bg-cover"
+            />
+          </div>
+          <div className="flex gap-5 fixed top-0 mt-5">
+            <button
+              onClick={() => downloadFile(imageUrl)}
+              className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transiotion-all duration-300"
+            >
+              <IoMdArrowRoundDown />
+            </button>
+            <button
+              onClick={() => {
+                setShowImage(false);
+                setImageUrl(null);
+              }}
+              className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transiotion-all duration-300"
+            >
+              <IoCloseSharp />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
